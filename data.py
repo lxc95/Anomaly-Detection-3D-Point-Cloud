83c43cb27ccfff0127d64cdc6560ffc76459339c
Code_for_paper <64560217+lxc95@users.noreply.github.com>
2023-02-18 22:28:35 -0500

    Add files via upload

--- data.py
+++ data.py
@@ -18,25 +18,13 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-def download():
-    BASE_DIR = os.path.dirname("../../")
-    DATA_DIR = os.path.join(BASE_DIR, 'Datasets')
-    if not os.path.exists(DATA_DIR):
-        os.mkdir(DATA_DIR)
-    if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
-        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
-        zipfile = os.path.basename(www)
-        os.system('wget %s; unzip %s' % (www, zipfile))
-        os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
-        os.system('rm %s' % (zipfile))
 
 def load_data(partition):
-    download()
-    BASE_DIR = os.path.dirname("../../")
+    BASE_DIR = os.path.dirname("/content/")
     DATA_DIR = os.path.join(BASE_DIR, 'Datasets')
     all_data = []
     all_label = []
-    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+    for h5_name in glob.glob(os.path.join(DATA_DIR, 'ply_data_%s*.h5' % partition)):
         f = h5py.File(h5_name)
         data = f['data'][:].astype('float32')
         label = f['label'][:].astype('int64')
@@ -49,32 +37,34 @@ def load_data(partition):
 
 
 def translate_pointcloud(pointcloud):
-    xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
+    xyz1 = np.random.uniform(low=2. / 3., high=3. / 2., size=[3])
     xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
-       
+
     translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
     return translated_pointcloud
 
-def pointcloud2graph(pointcloud, all_edges, threshold = 0.2):
+
+def pointcloud2graph(pointcloud, all_edges, threshold=0.2):
     N = all_edges.shape[0]
     dist = pointcloud.repeat((N, 1, 1))
-    dist = (dist - pointcloud.unsqueeze(1)).norm(dim = 2, keepdim = False)
-    k = threshold**2/2
+    dist = (dist - pointcloud.unsqueeze(1)).norm(dim=2, keepdim=False)
+    k = threshold ** 2 / 2
     mask = dist < threshold
-    dist = torch.exp(-dist**2 / k) * (mask).float()
+    dist = torch.exp(-dist ** 2 / k) * (mask).float()
     edges = all_edges[mask]
-    rowsum = dist.sum(dim = 1)
+    rowsum = dist.sum(dim=1)
     factor = 1. / rowsum[edges[:, 0]].float()
-    factor[factor>1] = 0.
-    #adj = torch.sparse.FloatTensor(edges.t(), torch.ones(edges.shape[0], dtype = torch.float) * factor, torch.Size([N,N]))
-    #return adj
-    return (edges, dist[mask] * factor) #edges and their values in adjacency matrix
+    factor[factor > 1] = 0.
+    # adj = torch.sparse.FloatTensor(edges.t(), torch.ones(edges.shape[0], dtype = torch.float) * factor, torch.Size([N,N]))
+    # return adj
+    return (edges, dist[mask] * factor)  # edges and their values in adjacency matrix
+
 
 class ModelNet40(Dataset):
     def __init__(self, num_points, partition='train'):
         self.data, self.label = load_data(partition)
         self.num_points = num_points
-        self.partition = partition 
+        self.partition = partition
 
     def __getitem__(self, item):
         pointcloud = self.data[item][:self.num_points]
